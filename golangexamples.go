ea7aab9438f9c75862d2428decaedffa0587586d
Rana ALi <7901492@gmail.com>
2019-04-06 21:29:55 +0500

    First commit

---
+++ golangexamples.go
@@ -0,0 +1,39 @@
+package golangexamples
+
+
+import "github.com/ehteshamz/greetings"
+
+func ConcatSlice(sliceToConcat []byte) string{
+	Sliced := ""
+	length:=len(sliceToConcat)
+	for i:=0;  i < length; i++{
+
+		if string(sliceToConcat[i])!=" " {
+			//fmt.Println(sliceToConcat[i])
+			Sliced += string(sliceToConcat[i])
+			if i!=length-1{
+				Sliced+="-"
+			}
+		}
+	}
+	return Sliced
+}
+
+func Encrypt(sliceToEncrypt	[]byte,	ceaserCount	int){
+	EncryptedString:=""
+	length:=len(sliceToEncrypt)
+	for i:=0; i<length; i++{
+		EncryptedString+=string(int(sliceToEncrypt[i])+ceaserCount)
+	}
+	fmt.Println("Encrypted String is: ")
+	fmt.Println(EncryptedString)
+}
+
+func EZGreetings(name string) string{
+
+		temp := greetings.PrintGreetings(name string)
+		return temp
+	
+	}
+	
+	
